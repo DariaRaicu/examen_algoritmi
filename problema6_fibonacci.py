f7cd46602280a95ca0f8347e1164922c003270bd
DariaRaicu <raicu.daria14@gmail.com>
2025-08-19 21:42:35 +0300

    Update problema6_fibonacci.py
    Rezolvare problema 6

--- problema6_fibonacci.py
+++ problema6_fibonacci.py
@@ -1,8 +1,17 @@
 # Problema 6 – Fibonacci recursiv (10p)
 
-def fib(n: int) -> int:
-    # TODO
-    return 0
+def fibonacci(n):
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    dp=[0] * (n+1)
+    dp[1]=1
+    for i in range(2,n+1):
+        dp[i]=dp[i-1]+dp[i-2]
+    return dp[n]
 
-if __name__ == "__main__":
-    print(fib(6))  # 8
+#Exemplu:
+n=7
+print(f"Fibonacci({n})=",fibonacci(n))
+#output: 13
